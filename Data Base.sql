adb27da8fe260efe77301c156ed0cfdf1947227d
Beanj10 <69943086+Beanj10@users.noreply.github.com>
2020-09-06 10:58:56 +1000

    Add files via upload

--- Data Base.sql
+++ Data Base.sql
@@ -24,25 +24,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`insulin`
+-- Table `mydb`.`needle assembly`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`insulin` (
-  `Insulin` INT NOT NULL,
-  `Blood_Sugar` INT NOT NULL,
-  `Log_Insulin` INT NOT NULL,
-  PRIMARY KEY (`Insulin`),
-  INDEX `fk_insulin_sensor_idx` (`Blood_Sugar` ASC) VISIBLE,
-  INDEX `fk_insulin_insulin1_idx` (`Log_Insulin` ASC) VISIBLE,
-  CONSTRAINT `fk_insulin_sensor`
-    FOREIGN KEY (`Blood_Sugar`)
-    REFERENCES `mydb`.`sensor` (`Sensor`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_insulin_insulin1`
-    FOREIGN KEY (`Log_Insulin`)
-    REFERENCES `mydb`.`insulin` (`Insulin`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `mydb`.`needle assembly` (
+  `Needle_ass` INT NOT NULL,
+  PRIMARY KEY (`Needle_ass`))
 ENGINE = InnoDB;
 
 
@@ -51,12 +37,74 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`pump` (
   `Pump` INT NOT NULL,
-  `Pump_Insulin` INT NOT NULL,
+  `needle_ass` INT NOT NULL,
   PRIMARY KEY (`Pump`),
-  INDEX `fk_pump_insulin1_idx` (`Pump_Insulin` ASC) VISIBLE,
-  CONSTRAINT `fk_pump_insulin1`
-    FOREIGN KEY (`Pump_Insulin`)
-    REFERENCES `mydb`.`insulin` (`Insulin`)
+  INDEX `fk_pump_needle assembly1_idx` (`needle_ass` ASC) VISIBLE,
+  CONSTRAINT `fk_pump_needle assembly1`
+    FOREIGN KEY (`needle_ass`)
+    REFERENCES `mydb`.`needle assembly` (`Needle_ass`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`alarm`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`alarm` (
+  `Alarm` INT NOT NULL,
+  `Battery low` INT NULL,
+  `Sensor failure` VARCHAR(45) NULL,
+  `Pump failure` VARCHAR(45) NULL,
+  `Delivery failure` VARCHAR(45) NULL,
+  `Needle assembly removed` VARCHAR(45) NULL,
+  `Insulin reservior removed` VARCHAR(45) NULL,
+  `Low insulin level` VARCHAR(45) NULL,
+  PRIMARY KEY (`Alarm`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`display`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`display` (
+  `Display` INT NOT NULL,
+  PRIMARY KEY (`Display`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`controller`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`controller` (
+  `ContolerId` INT NOT NULL,
+  `Pump` INT NOT NULL,
+  `Alarm` INT NOT NULL,
+  `Display` INT NOT NULL,
+  `Sensor` INT NOT NULL,
+  PRIMARY KEY (`ContolerId`),
+  INDEX `fk_controller_pump_idx` (`Pump` ASC) VISIBLE,
+  INDEX `fk_controller_Alarm1_idx` (`Alarm` ASC) VISIBLE,
+  INDEX `fk_controller_display1_idx` (`Display` ASC) VISIBLE,
+  INDEX `fk_controller_sensor1_idx` (`Sensor` ASC) VISIBLE,
+  CONSTRAINT `fk_controller_pump`
+    FOREIGN KEY (`Pump`)
+    REFERENCES `mydb`.`pump` (`Pump`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_controller_Alarm1`
+    FOREIGN KEY (`Alarm`)
+    REFERENCES `mydb`.`alarm` (`Alarm`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_controller_display1`
+    FOREIGN KEY (`Display`)
+    REFERENCES `mydb`.`display` (`Display`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_controller_sensor1`
+    FOREIGN KEY (`Sensor`)
+    REFERENCES `mydb`.`sensor` (`Sensor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
